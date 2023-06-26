1a5a5ff273fe7dec2e8a5c4280230ae4784c430b
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-06-26 15:36:57 -0500

    Sumar numeros con templates

--- project/Destinos/calc/views.py
+++ project/Destinos/calc/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{})
+    return render(request,'home.html',{'name': 'Christian'})
+
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    res=val1+val2
+    return render(request,'result.html',{'result':res})
