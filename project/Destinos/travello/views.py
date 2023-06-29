f846f8801e67eac4c37de437d6d71c85cc35ae03
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-06-28 21:46:30 -0500

    agregar offer al modelo y utilizar for e if en html

--- project/Destinos/travello/views.py
+++ project/Destinos/travello/views.py
@@ -6,5 +6,24 @@ def index(request):
     dest1=Destination()
     dest1.name="Mumbai"
     dest1.desc="The best city for visit in the summer"
+    dest1.img="destination_1.jpg"
     dest1.price=700
-    return render(request,'index.html',{'dest1': dest1})
+    dest1.offer=True
+
+    dest2=Destination()
+    dest2.name="Paris"
+    dest2.desc="The city of love"
+    dest2.img="destination_2.jpg"
+    dest2.price=680
+    dest2.offer=False
+
+    dest3=Destination()
+    dest3.name="Hawai"
+    dest3.desc="Beautiful city"
+    dest3.img="destination_3.jpg"
+    dest3.price=750
+    dest3.offer=True
+
+    dests=[dest1, dest2, dest3]
+
+    return render(request,'index.html',{'dests': dests})
