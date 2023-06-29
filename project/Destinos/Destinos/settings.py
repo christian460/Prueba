f99dbc63f17c512f7e855a346f7df16474ccd2be
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-06-29 00:20:42 -0500

    Agregar un usuario

--- project/Destinos/Destinos/settings.py
+++ project/Destinos/Destinos/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calc',
 ]
 
 MIDDLEWARE = [
@@ -127,7 +126,11 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
