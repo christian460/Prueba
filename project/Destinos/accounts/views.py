01af2b7fcdfa424266a81b600edfcf59742f8ce3
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-06-29 01:32:49 -0500

    correccion de errores, iniciar secion y cerrar cesion

--- project/Destinos/accounts/views.py
+++ project/Destinos/accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def register(request):
-
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -14,15 +14,38 @@ def register(request):
 
         if password1==password2:
             if User.objects.filter(username=username).exists():
-                print('Usuario existente')
-            elif User.object.filter(email=email).exists():
-                print('Email existente')
+                messages.info(request, 'Usuario existente')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email existente')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
-                print ('Usuario creado')
+                print('Usuario creado')
+                return redirect('login')
         else:
-            print ('Password no coincide')
+            messages.info(request,'Password no coincide')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'Datos invalidos') 
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
